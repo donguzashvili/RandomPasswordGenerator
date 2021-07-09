4c501a84029575542002bad485d26261e603bd51
shota donguzashvili <shota.nope@gmail.com>
2021-07-09 04:32:30 +0400

    feat: audio volume down

--- src/Components/RainAnimation.jsx
+++ src/Components/RainAnimation.jsx
@@ -3,10 +3,15 @@ import "./RainAnimation.css";
 import RainSound from "../assets/audio/sound.mp3";
 
 export default function RainEffect() {
+  //audio sound is too loud so made it lower
+  window.onload = () => {
+    let audio = document.getElementById("audio");
+    audio.volume = 0.01;
+  };
   return (
     <div>
       <div className="rain"></div>
-      <audio controls>
+      <audio id="audio" controls>
         <source src={RainSound} type="audio/mpeg" />
       </audio>
     </div>
